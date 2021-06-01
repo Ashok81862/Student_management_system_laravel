7ab46e1349eee7a581a34dff1d4c304051ca5670
Ashok812 <chhetriarash81@gmail.com>
2021-06-01 12:32:28 +0545

    User Controller and Migration Created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('User');
+            $table->foreignId('media_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
